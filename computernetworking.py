16a5f4f3dbcbfd5345f5052a69ff8159c5b10ea9
Moses <moses.s.troyer@gmail.com>
2015-11-04 20:56:07 -0500

    Initial commit

---
+++ computernetworking.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+application = Flask(__name__)
+application.config['SECRET_KEY'] = "secret"
+socketio = SocketIO(application)
+
+@application.route("/")
+def hello():
+	return render_template("index.html")
+
+@socketio.on("connect", namespace="/")
+def test_connect():
+	print("someone connected")
+
+@socketio.on("chatMessage", namespace="/")
+def chat_message(data):
+	emit("chatMessage", data, broadcast=True) 
+
+if __name__ == "__main__":
+	#application.host = "0.0.0.0"
+	socketio.run(application, host='0.0.0.0')
+	#application.run(host="0.0.0.0")
